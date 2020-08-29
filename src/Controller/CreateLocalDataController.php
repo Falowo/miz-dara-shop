3e92f8fba2f3ac8795088e580a52df765192cb2d
Josselin Krikorian <obarayego@gmail.com>
2020-08-29 02:06:17 +0200

    create local data

---
+++ src/Controller/CreateLocalDataController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Continent;
+use App\Entity\Country;
+use App\Entity\NgState;
+use App\Service\Locale\LocaleService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CreateLocalDataController extends AbstractController
+{
+    /**
+     * @Route("/create/local/data", name="create_local_data")
+     */
+    public function index(LocaleService $localeService, EntityManagerInterface $em)
+    {
+        $continents = $localeService->getContinentConst();
+
+        foreach ($continents as $k=>$v){
+            $continent = new Continent();
+            $continent->setCode($k);
+            $em->persist($continent);
+        }
+        $em->flush();
+
+        $countries = $localeService->getArrayCountries();
+
+        foreach($countries as $k=>$v){
+            $country = new Country();
+            $country->setCode($k);
+            $em->persist($country);
+        }
+        $em->flush();
+
+        $ngStates = $localeService->getNgStateConst();
+
+        foreach($ngStates as $k=>$v){
+            $ngState = new NgState();
+            $ngState->setJsonId($k);
+            $em->persist($ngState);
+        }
+        $em->flush();
+
+
+        return $this->render('create_local_data/index.html.twig', [
+            'controller_name' => 'CreateLocalDataController',
+        ]);
+    }
+}
