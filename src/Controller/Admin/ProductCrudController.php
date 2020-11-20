dc03425eb48b7f179b3f528195e66e1ea2750691
Josselin Krikorian <obarayego@gmail.com>
2020-11-20 16:53:17 +0100

    test13

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -65,9 +65,9 @@ class ProductCrudController extends AbstractCrudController
         } elseif (Crud::PAGE_DETAIL === $pageName) {
             return [$name, $info, $tags, $categories, $stocks, $mainImage, $images];
         } elseif (Crud::PAGE_NEW === $pageName) {
-            return [$panel1, $name, $info, $tags, $price, $discountPrice, $categories, $panel2, $mainImageFile, $images, $panel3, $stocks];
+            return [$panel1, $name, $info, $tags, $price, $discountPrice, $categories, $panel2, $mainImageFile, $panel3, $stocks];
         } elseif (Crud::PAGE_EDIT === $pageName) {
-            return [$panel1, $name, $info, $tags, $price, $discountPrice, $categories, $panel2, $mainImage, $mainImageFile, $images, $panel3, $stocks];
+            return [$panel1, $name, $info, $tags, $price, $discountPrice, $categories, $panel2, $mainImage, $mainImageFile, $panel3, $stocks];
         }
     }
 
