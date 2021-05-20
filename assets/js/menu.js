ea3dcb0b41b283a924f90a41d717249a12c187a9
josselin <obarayego@gmail.com>
2021-05-20 09:29:29 +0200

    makking sure dom is fully loaded in menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,23 +1,23 @@
 const secondLevelElts = document.querySelectorAll(".second-level");
 const thirdLevelElts = document.querySelectorAll(".third-level");
 
+window.addEventListener("DOMContentLoaded", (event) => {
+	console.log("DOM fully loaded and parsed");
 
-setTimeout(
-  () =>
-    thirdLevelElts.forEach((element) => {
-      element.classList.add("d-none");
-    }),
-  100
-);
+	setTimeout(
+		() =>
+			thirdLevelElts.forEach((element) => {
+				element.classList.add("d-none");
+			}),
+		100
+	);
 
-if (window.innerWidth<768) {
-  setTimeout(
-    () => {       
-      secondLevelElts.forEach((element) => {
-        element.classList.remove("d-flex");
-        element.classList.add("d-none");
-      })
-    },
-    200
-  );
-}
+	if (window.innerWidth < 768) {
+		setTimeout(() => {
+			secondLevelElts.forEach((element) => {
+				element.classList.remove("d-flex");
+				element.classList.add("d-none");
+			});
+		}, 200);
+	}
+});
